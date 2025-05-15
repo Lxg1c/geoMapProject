3891c24e41a4ac15169f51ee5823ba7a6877a44c
Lxg1c <teran_2005@mail.ru>
2025-05-15 18:19:28 +0300

    initialized create report page

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,5 +1,9 @@
-function CreateReport() {
-    return
+export default function CreatePage() {
+    return(
+        <section className="create">
+            <div className='create__container container'>
+                CreatePage
+            </div>
+        </section>
+    )
 }
-
-export default CreateReport
