8af2dbc9d44aad3523a30b94e0141fd8125be40c
Lxg1c <teran_2005@mail.ru>
2025-05-17 16:27:09 +0300

    finished createPage

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,8 +1,41 @@
+import useUserStore from "@/shared/store/store";
+import {YMaps} from "@pbe/react-yandex-maps";
+import GeoMap from "@/features/map/GeoMap";
+import {useState} from "react";
+import {InfoWrapper} from "@/shared/ui/InfoWrapper/InfoWrapper";
+import {Preferences} from "@/features/preferences/Preferences";
+import {UserInfo} from "@/features/userInfo/userInfo";
+
+
 export default function CreatePage() {
+    const username = useUserStore(state => state.username);
+    const phone = useUserStore(state => state.phone);
+    const preferences = useUserStore(state => state.preferences)
+    const [scanRadius, setScanRadius] = useState<number>(1000);
+
+
     return(
-        <section className="create">
-            <div className='create__container container'>
-                CreatePage
+        <section className='create mt-5 relative z-10'>
+            <div className='create__container container flex items-center justify-between'>
+                <div className='create__container-left max-w-90 w-full'>
+                    <InfoWrapper>
+                        <UserInfo
+                            username={username}
+                            phone={phone}
+                        />
+                        <Preferences
+                            preferenceList={preferences}
+                            scanRadius={scanRadius}
+                            setScanRadius={setScanRadius}
+                        />
+                    </InfoWrapper>
+                </div>
+
+                <div className="create__container-right w-187.5 h-150 rounded-2xl">
+                    <YMaps query={{ apikey: import.meta.env.VITE_MAP_PUBLIC_KEY }}>
+                        <GeoMap scanRadius={scanRadius} />
+                    </YMaps>
+                </div>
             </div>
         </section>
     )
